f40f6e01b1a372c203a1adb2363d850287c712be
ManiaDaneshfar <maniadaneshfar@outlook.com>
2021-06-09 17:39:24 +0430

    Add project files.

---
+++ Blog/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Blog.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Blog.Areas.Identity.IdentityHostingStartup))]
+namespace Blog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlogContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BlogContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlogContext>();
+            });
+        }
+    }
+}
